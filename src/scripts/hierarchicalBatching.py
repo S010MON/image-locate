5e6f042ba62eba5b1a5fe9a0b014cfb1acf5930a
leon <leon_debnath@hotmail.co.uk>
2023-05-31 15:25:39 +0200

    Fix making directories for batches

--- src/scripts/hierarchicalBatching.py
+++ src/scripts/hierarchicalBatching.py
@@ -25,7 +25,6 @@ output_path_gnd = "/tf/CVUSA/hier_batch/gnd"
 file_names = os.listdir(input_path_sat)
 embeddings = np.zeros((len(file_names), 32768))
 
-print("Encode embeddings")
 
 # Encode embeddings for all images in the folder
 for i, file_name in enumerate(tqdm(file_names)):
@@ -42,7 +41,6 @@ kmeans = MiniBatchKMeans(n_clusters=n_clusters,
 preds = kmeans.fit_predict(embeddings)
 
 # Write to file just in case / for visualisation
-print("Write to file")
 with open("hierarchical_batches.csv", "w") as file:
     file.write("file_name,prediction,dims")
     for i, file_name in enumerate(tqdm(file_names)):
@@ -64,9 +62,9 @@ for n in range(n_clusters):
 # Copy files over to new homes
 for i, file_name in enumerate(tqdm(file_names)):
     src_sat = os.path.join(input_path_sat, file_name)
-    dest_sat = os.path.join(output_path_sat, file_name)
+    dest_sat = os.path.join(output_path_sat, str(preds[i]), file_name)
     shutil.copy2(src_sat, dest_sat)
 
     src_gnd = os.path.join(input_path_gnd, file_name)
-    dest_gnd = os.path.join(output_path_gnd, file_name)
-    shutil.copy2(src_sat, dest_sat)
+    dest_gnd = os.path.join(output_path_gnd, str(preds[i]), file_name)
+    shutil.copy2(src_gnd, dest_gnd)
